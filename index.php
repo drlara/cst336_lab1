e9598d3eb6776b150d3f4ded7389b8f87311e54a
EC2 Default User <ec2-user@ip-172-31-1-123.us-west-1.compute.internal>
2020-10-24 23:24:50 +0000

    adding first page

--- index.php
+++ index.php
@@ -1,11 +1,41 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title> Lab 1: Personal Page </title>
+        <meta charset="utf-8">
     </head>
     <body>
         
-        <h1>Welcome to my website!</h1>
+        <header>
+            <h1> Miguel Lara </h1>
+        </header>
+        
+        <hr>
+        
+        <nav>
+            
+            <a href="index.html"> Home </a>
+            <a href="about.html"> About </a>
+            <a href="contact.html"> Contact Me </a>
+            
+        </nav>
+        
+        <main>
+            
+            <figure>
+                <img src="img/juan_doe.png">
+            </figure>
+            
+            <div>
+              Welcome to my site!
+            </div>
+            
+        </main>
 
+
+        <footer>
+            
+        </footer>
+        
     </body>
 </html>
